52a298302fee1ef3e978cb550f8571957e9fd546
keppuraja3 <keppuraja3@gmail.com>
2024-07-12 18:29:47 +0530

    background image adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { motion } from "framer-motion"
+import NavBar from './component/NavBar'
 function App() {
   return (
     <>
+    <NavBar/>
     <h1>Portfolio</h1>
     </>
   )
